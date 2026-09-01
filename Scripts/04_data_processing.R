2ee1ed8fda3b579550657dba582b48093d60569b
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-09-01 13:34:28 -0400

    feat: convert to 2017 onward for Medicaid disagg trends

--- Scripts/04_data_processing.R
+++ Scripts/04_data_processing.R
@@ -756,7 +756,8 @@ subpop_medicaid <- c("Children", "Medicaid Expansion Adults", "Dual Eligible")
 df_medicaid_disagg_trend <- df_ff |>
   filter(
     area == "Medicaid & CHIP",
-    sub_category %in% subpop_medicaid
+    sub_category %in% subpop_medicaid,
+    data_year >= 2017
   ) |>
   select(
     area,
