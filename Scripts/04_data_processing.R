3bea7624d9d4455e02d9b7e5a7bc2f0986f7db7e
Aaron Chafetz <aaron.chafetz@cms.hhs.gov>
2026-09-09 12:48:22 -0400

    fix:recolor part A to match uitlization and increase contrast

--- Scripts/04_data_processing.R
+++ Scripts/04_data_processing.R
@@ -931,10 +931,10 @@ df_cs_trend <- df_cs_trend |>
       )(value)
     ),
     fill_color = case_when(
-      category == "Part A" ~ ff_colors$scales$cobolt[["200"]],
+      category == "Part A" ~ ff_colors$scales$cobolt[["300"]],
       category == "Part B" ~ ff_colors$scales$cobolt[["700"]],
       category == "Part D" ~ ff_colors$scales$cobolt[["900"]],
-      str_detect(sub_category, "Part A") ~ ff_colors$scales$cobolt[["200"]],
+      str_detect(sub_category, "Part A") ~ ff_colors$scales$cobolt[["300"]],
       str_detect(sub_category, "Part B") ~ ff_colors$scales$cobolt[["700"]],
     )
   ) |>
